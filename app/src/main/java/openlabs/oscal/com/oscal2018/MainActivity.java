f93c57189868e356a79dc4d830b783b489c552f4
AndyScherzinger <info@andy-scherzinger.de>
2018-05-08 12:46:28 +0200

    fix back navigation

--- app/src/main/java/openlabs/oscal/com/oscal2018/MainActivity.java
+++ app/src/main/java/openlabs/oscal/com/oscal2018/MainActivity.java
@@ -5,39 +5,48 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-    public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-        private WebView myWebView;
+    private WebView webView;
 
-        @SuppressLint("SetJavaScriptEnabled")
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.webview);
-        myWebView.loadUrl("https://app.oscal.openlabs.cc");
-        WebSettings webSettings = myWebView.getSettings();
+        webView = findViewById(R.id.webview);
+        webView.loadUrl("https://app.oscal.openlabs.cc");
+        WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new MyWebViewClient());
+        webView.setWebViewClient(new MyWebViewClient());
     }
 
-        private class MyWebViewClient extends WebViewClient {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                if (Uri.parse(url).getHost().equals("app.oscal.openlabs.cc")) {
-                    // This is my web site, so do not override; let my WebView load the page
-                    return false;
-                }
-                // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-                startActivity(intent);
-                return true;
-            }
+    @Override
+    public void onBackPressed() {
+        if (webView.copyBackForwardList().getCurrentIndex() > 0) {
+            webView.goBack();
+        } else {
+            // Your exit alert code, or alternatively line below to finish
+            super.onBackPressed(); // finishes activity
         }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("app.oscal.openlabs.cc")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
 }
